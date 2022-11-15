e0e5ee2ee29c0aeff9112426c711d929c519bc04
Solyahne <m.antignac@gmail.com>
2022-11-15 22:19:04 +0100

    Mise en page page séries photographies

--- src/pages/photographies/photographies.tsx
+++ src/pages/photographies/photographies.tsx
@@ -5,15 +5,18 @@ import "./photographies.css";
 export default function Photographies(): JSX.Element {
     return (
         <main className="photographies_series_main">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu mi viverra, elementum libero euismod, sagittis neque. In sit amet justo sodales, hendrerit ipsum et, imperdiet nunc. Etiam vel erat hendrerit, fermentum risus ac, interdum est. Praesent varius in magna a auctor. Aliquam erat volutpat. Nunc mattis aliquam magna eget elementum. Nunc non mi nec justo euismod condimentum. Integer posuere sed quam sed tristique.
+            <p className="photographies_introduction">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Duis eu mi viverra, elementum libero euismod, sagittis neque.
+                In sit amet justo sodales, hendrerit ipsum et, imperdiet nunc. Etiam vel erat hendrerit, fermentum risus ac, interdum est. Praesent varius in magna a auctor. Aliquam erat volutpat. Nunc mattis aliquam magna eget elementum. Nunc non mi nec justo euismod condimentum. Integer posuere sed quam sed tristique.
+            </p>
             <section className="photographies_cards_container">
-            {photocover.map((element) => (
-            <PhotoSeries 
-            cover={element.picture}
-            alt={element.alt}
-            description={element.description}
-            />
-            ))}
+                {photocover.map((element) => (
+                    <PhotoSeries
+                        cover={element.picture}
+                        alt={element.alt}
+                        description={element.description}
+                    />
+                ))}
             </section>
         </main>
     )
